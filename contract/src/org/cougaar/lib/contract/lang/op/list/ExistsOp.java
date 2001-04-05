801d3310ef4e9fd90e00f997d2b30d120a409995
mthome <mthome>
2001-04-05 19:28:32 +0000

    Update copyright notices in simple cases

--- contract/src/org/cougaar/lib/contract/lang/op/list/ExistsOp.java
+++ contract/src/org/cougaar/lib/contract/lang/op/list/ExistsOp.java
@@ -1,10 +1,10 @@
 /*
  * <copyright>
- *  Copyright 1999-2000 Defense Advanced Research Projects
- *  Agency (DARPA) and ALPINE (a BBN Technologies (BBN) and
- *  Raytheon Systems Company (RSC) Consortium).
- *  This software to be used only in accordance with the
- *  COUGAAR licence agreement.
+ * Copyright 1997-2001 Defense Advanced Research Projects
+ * Agency (DARPA) and ALPINE (a BBN Technologies (BBN) and
+ * Raytheon Systems Company (RSC) Consortium).
+ * This software to be used only in accordance with the
+ * COUGAAR licence agreement.
  * </copyright>
  */
 
