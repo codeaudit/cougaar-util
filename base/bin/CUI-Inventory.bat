3a1bbdf8c94bd2aecacb45485eef69f51e0f33ff
jwinston <jwinston>
2001-08-22 13:03:55 +0000

    bat script for Pat Clark inventory gui

---
+++ base/bin/CUI-Inventory.bat
@@ -0,0 +1,13 @@
+@echo OFF
+
+REM calls setlibpath.bat which sets the path to the required jar files.
+CALL %COUGAAR_INSTALL_PATH%\bin\setlibpath.bat
+CALL %COUGAAR_INSTALL_PATH%\bin\setarguments.bat
+
+REM produces the inventory chart display
+set MYCLASSES=org.cougaar.lib.uiframework.ui.inventory.InventoryChartUI
+set BS=org.cougaar.core.society.Bootstrapper
+@ECHO ON
+
+java.exe %MYPROPERTIES% %MYMEMORY% -classpath %LIBPATHS% %BS% %MYCLASSES% %1
+
