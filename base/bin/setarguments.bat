5a7bf8e547659c987535e585f07355b49d4f03fe
mthome <mthome>
2001-11-20 20:14:03 +0000

    Clean out some old (useless) settings and over-quieting of warnings

--- base/bin/setarguments.bat
+++ base/bin/setarguments.bat
@@ -26,7 +26,7 @@ rem set MYDOMAINS=-Dorg.cougaar.domain.alp=org.cougaar.domain.glm.GLMDomain
 set MYDOMAINS=
 set MYCLASSES=org.cougaar.core.society.Node
 
-set MYPROPERTIES=-Dorg.cougaar.system.path=%COUGAAR3RDPARTY% -Dorg.cougaar.install.path=%COUGAAR_INSTALL_PATH% -Duser.timezone=GMT -Dorg.cougaar.core.cluster.startTime=08/10/2005 -Dorg.cougaar.domain.planning.ldm.lps.ComplainingLP.level=0 -Dorg.cougaar.core.cluster.SharedPlugInManager.watching=false
+set MYPROPERTIES=-Dorg.cougaar.system.path=%COUGAAR3RDPARTY% -Dorg.cougaar.install.path=%COUGAAR_INSTALL_PATH% -Duser.timezone=GMT -Dorg.cougaar.core.cluster.startTime=08/10/2005
 
 set MYMEMORY=-Xms100m -Xmx300m
 
