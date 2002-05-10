6cdec29e72747070496280d432c686018bc35b8e
mthome <mthome>
2002-05-10 18:54:53 +0000

    Add a simple base class for building trivial components. Little more than GenericStateModelAdapter

---
+++ util/src/org/cougaar/core/component/ComponentSupport.java
@@ -0,0 +1,44 @@
+/*
+ * <copyright>
+ *  Copyright 2002 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+package org.cougaar.core.component;
+
+import java.util.*;
+import org.cougaar.util.GenericStateModelAdapter;
+
+/** ComponentSupport is a shell of a ComponentImplementation
+ * which is not actually instantiable, but may be useful
+ * as a base class for simple component implementations.
+ **/
+public class ComponentSupport 
+  extends GenericStateModelAdapter
+  implements Component
+{
+  private BindingSite _bindingSite = null;
+  public void setBindingSite(BindingSite bs) {
+    _bindingSite = bs;
+  }
+  protected final BindingSite getBindingSite() {
+    return _bindingSite;
+  }
+  protected final ServiceBroker getServiceBroker() {
+    return getBindingSite().getServiceBroker();
+  }
+}
