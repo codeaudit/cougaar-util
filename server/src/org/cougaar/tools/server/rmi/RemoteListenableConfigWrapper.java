e506664f714c9b44272e33d03188a2e4c8bcfb62
mthome <mthome>
2003-01-23 19:36:52 +0000

    Update copyright notices

--- server/src/org/cougaar/tools/server/rmi/RemoteListenableConfigWrapper.java
+++ server/src/org/cougaar/tools/server/rmi/RemoteListenableConfigWrapper.java
@@ -1,12 +1,12 @@
 /*
  * <copyright>
- *  Copyright 1997-2001 BBNT Solutions, LLC
+ *  Copyright 1997-2003 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
- *
+ * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
  *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
- *
+ * 
  *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
  *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
  *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
