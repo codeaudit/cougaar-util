b3853ce57f0e4c7f80bd521e1bd8941e4bc8efe1
mthome <mthome>
2004-04-07 13:07:04 +0000

    Add GC utility, factored out of earlier IdentityTable patch (for cbug 3555)

---
+++ util/src/org/cougaar/util/GC.java
@@ -0,0 +1,83 @@
+/*
+ * <copyright>
+ *  
+ *  Copyright 2004 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects
+ *  Agency (DARPA).
+ * 
+ *  You can redistribute this software and/or modify it under the
+ *  terms of the Cougaar Open Source License as published on the
+ *  Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ * </copyright>
+ */
+
+package org.cougaar.util;
+
+import org.cougaar.util.log.Logger;
+import org.cougaar.util.log.Logging;
+import org.cougaar.util.PropertyParser;
+
+/**
+ * A utility for requesting a GC in various ways.
+ */
+public class GC {
+  // uninstantiable
+  private GC() {}
+
+  /** Static logger for GC **/
+  private static Logger log = Logging.getLogger(GC.class);
+
+  /** lock for GC timer **/
+  private static final Object gcLock = new Object();
+
+  /** when did we last do a GC, guarded by gcLock **/
+  private static long gcTime = 0L;
+
+  public static final long minGCInterval_DEFAULT = 3000000L;
+  public static final String minGCInterval_DEFPROP = "org.cougaar.core.persistence.lazyInterval";
+  public static final String minGCInterval_PROP = "org.cougaar.util.GC.minGCInterval";
+  private static final long minGCInterval = 
+    PropertyParser.getLong(minGCInterval_PROP, PropertyParser.getLong(minGCInterval_DEFPROP, minGCInterval_DEFAULT));
+
+  /** Invoke System.gc(), subject to policy constraints:
+   * e.g. if it has been at least minGCInterval since
+   * the last gc call completed.
+   *
+   * @property org.cougaar.util.GC.minGCInterval
+   * Minimum number of milliseconds between IdentityTable-directed gcs.
+   * Defaults to 3000000 (5 minutes).  Garbage Collections not invoked
+   * through this interface are invisible.  The default value 
+   * is actually supplied by the property org.cougaar.core.persistence.lazyInterval,
+   * if available.
+   **/
+  public static final void gc() {
+    long now = System.currentTimeMillis();
+    synchronized (gcLock) {
+      if ((gcTime != 0L) &&
+          ((now-gcTime)<minGCInterval)) {
+        return;
+      } 
+
+      System.gc();
+
+      long tend = System.currentTimeMillis();
+      gcTime = tend;
+      if (log.isInfoEnabled()) {
+        log.info("GC.gc() ran for "+(tend-now)+" millis");
+      }
+    }
+  }
+}
