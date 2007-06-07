fd2048026f631c9db8e039a162de1e61673eace6
twright <twright>
2007-06-07 20:10:16 +0000

    Added TestArguments to the test suite.

--- util/regress/org/cougaar/util/UtilTest.java
+++ util/regress/org/cougaar/util/UtilTest.java
@@ -36,6 +36,7 @@ public class UtilTest extends TestCase {
 
   public static Test suite() {
     TestSuite suite= new TestSuite("Cougaar Utility Class Tests");
+    suite.addTest(new TestSuite(TestArguments.class));
     suite.addTest(new TestSuite(TestCircularQueue.class));
     suite.addTest(new TestSuite(TestDBProperties.class));
     suite.addTest(new TestSuite(TestMappings.class));
