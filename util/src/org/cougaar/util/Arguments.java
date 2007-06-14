4d468f83d72bb2e43330acc8d149c0008e479a07
rshapiro <rshapiro>
2007-06-14 14:16:48 +0000

    Refix javadoc errors that I thought I already fixed

--- util/src/org/cougaar/util/Arguments.java
+++ util/src/org/cougaar/util/Arguments.java
@@ -63,12 +63,12 @@ import org.cougaar.bootstrap.SystemProperties;
  * Example use:
  * 
  * <pre>
- *   Arguments args = new Arguments(&quot;x=y, q=one, q=two, z=99&quot;);
- *   String x = args.getString(&quot;x&quot;);
- *   assert &quot;y&quot;.equals(x);
- *   int z = args.getInt(&quot;z&quot;, 1234);
+ *   Arguments args = new Arguments("x=y, q=one, q=two, z=99");
+ *   String x = args.getString("x");
+ *   assert "y".equals(x);
+ *   int z = args.getInt("z", 1234);
  *   assert z == 99;
- *   List&lt;String&gt; q = args.getStrings(&quot;q&quot;);
+ *   List&lt;String&gt; q = args.getStrings("q");
  *   assert q != null &amp;&amp; q.size() == 2;
  * </pre>
  * 
@@ -80,20 +80,20 @@ import org.cougaar.bootstrap.SystemProperties;
  *   package org;
  *   public class MyPlugin ... {
  *     private Arguments args;
- *     // The &quot;setArguments&quot; method is special -- it's an optional method
+ *     // The "setArguments" method is special -- it's an optional method
  *     // that's found by the component model via reflection.  The passed-in
  *     // arguments instance is created via:
  *     //    new Arguments(listOfStrings, classname);
  *     public void setArguments(Arguments args) { this.args = args; }
  *     public void load() {
  *       super.load();
- *       // Get the value of our &quot;foo&quot; argument
+ *       // Get the value of our "foo" argument
  *       //
- *       // First looks for a plugin XML argument named &quot;foo&quot;,
- *       // next looks for a &quot;-Dorg.MyPlugin.foo=&quot; system property,
+ *       // First looks for a plugin XML argument named "foo",
+ *       // next looks for a "-Dorg.MyPlugin.foo=" system property,
  *       // otherwise the value will be the 1234 default.
- *       int foo = args.getInt(&quot;foo&quot;, 1234);
- *       System.out.println(&quot;foo is &quot;+foo);
+ *       int foo = args.getInt("foo", 1234);
+ *       System.out.println("foo is "+foo);
  *     }
  *   }
  * </pre>
@@ -106,12 +106,12 @@ import org.cougaar.bootstrap.SystemProperties;
  *   public class MyPlugin ... {
  *     private int foo = 1234;
  *     public void setArguments(Arguments args) { args.callSetters(this); }
- *     // This &quot;set&lt;i&gt;NAME&lt;/i&gt;(&lt;i&gt;TYPE&lt;/i&gt;)&quot; method is found by reflection.
+ *     // This "set<i>NAME</i>(<i>TYPE</i>)" method is found by reflection.
  *     // The args class will only invoke the setters for which it has values.
  *     public void setFoo(int i) { this.foo = i; }
  *     public void load() {
  *       super.load();
- *       System.out.println(&quot;foo is &quot;+foo);
+ *       System.out.println("foo is "+foo);
  *     }
  *   }
  * </pre>
@@ -373,9 +373,9 @@ public final class Arguments extends AbstractMap<String, List<String>> implement
      * 
      * <pre>
      * For example, the constructor input:
-     *   &quot;foo=f1, bar=b1, qux=q1,
+     *   "foo=f1, bar=b1, qux=q1,
      *    foo=f2, bar=b2, qux=q2,
-     *    foo=f3, bar=b3, qux=q3&quot;
+     *    foo=f3, bar=b3, qux=q3"
      * will be parsed as:
      *   {foo=[f1,f2,f3], bar=[b1,b2,b3], qux=[q1,q2,q3]}
      * and can be split into:
@@ -384,7 +384,7 @@ public final class Arguments extends AbstractMap<String, List<String>> implement
      *    {foo=[f3], bar=[b3], qux=[q3]}}
      * This simplifies iteration:
      *   for (Arguments a : args.split()) {
-     *     System.out.println(&quot;foo is &quot;+a.getString(&quot;foo&quot;));
+     *     System.out.println("foo is "+a.getString("foo"));
      *   }
      * which will print:
      *   foo is f1
@@ -526,13 +526,13 @@ public final class Arguments extends AbstractMap<String, List<String>> implement
      * For example, the name=value pair "x=y" will look for:
      * 
      * <pre>
-     *   public void setX(&lt;i&gt;type&lt;/i&gt;) {..}
+     *   public void setX(<i>type</i>) {..}
      * </pre>
      * 
      * and field:
      * 
      * <pre>
-     *   public &lt;i&gt;type&lt;/i&gt; x;
+     *   public <i>type</i> x;
      * </pre>
      * 
      * If neither are found then "x" will be included in the returned Set.
@@ -669,20 +669,20 @@ public final class Arguments extends AbstractMap<String, List<String>> implement
      *   A=B
      *   X=V0,V1,V2
      * then:
-     *   toString(&quot;the_$key is the_$value&quot;, &quot; * &quot;);
+     *   toString("the_$key is the_$value", " * ");
      * would return:
      *   the_A is the_B * the_X is the_V0
      * and:
-     *   toString(&quot;($key eq $vals)&quot;, &quot; +\n&quot;);
+     *   toString("($key eq $vals)", " +\n");
      * would return:
      *   (A eq B) +
      *   (X eq [V0, V1, V2])
      * and:
-     *   toString(&quot;$key=$veach&quot;, &quot;&amp;&quot;);
+     *   toString("$key=$veach", "&amp;");
      * would return a URL-like string:
      *   A=B&amp;X=V0&amp;X=V1&amp;X=V2
      * and:
-     *   &quot;{&quot; + toString(&quot;$key=$vlist&quot;, &quot;, &quot;) + &quot;}&quot;;
+     *   "{" + toString("$key=$vlist", ", ") + "}";
      * would return the standard {@link Map#toString} format:
      *   {A=[B], X=[V0, V1, V2]}
      * </pre>
