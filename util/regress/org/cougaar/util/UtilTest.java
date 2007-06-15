16839832d6dbc03813f8546ce72b63e2f546de7d
rshapiro <rshapiro>
2007-06-15 21:05:43 +0000

    Add annotation test suite

--- util/regress/org/cougaar/util/UtilTest.java
+++ util/regress/org/cougaar/util/UtilTest.java
@@ -37,6 +37,7 @@ public class UtilTest extends TestCase {
   public static Test suite() {
     TestSuite suite= new TestSuite("Cougaar Utility Class Tests");
     suite.addTest(new TestSuite(TestArguments.class));
+    suite.addTest(new TestSuite(TestAnnotations.class));
     suite.addTest(new TestSuite(TestCircularQueue.class));
     suite.addTest(new TestSuite(TestDBProperties.class));
     suite.addTest(new TestSuite(TestMappings.class));
