67636c0fe0d78bc215ad9d9549bf506ab6a29d25
rshapiro <rshapiro>
2007-08-28 15:08:38 +0000

    New UnaryPredicate for dynamic instance-of

---
+++ util/src/org/cougaar/util/IsInstanceOf.java
@@ -0,0 +1,23 @@
+/*
+ *
+ * Copyright 2007 by BBN Technologies Corporation
+ *
+ */
+
+package org.cougaar.util;
+
+/**                                                                         
+ * General purpose dynamic 'instanceof' predicate.                                   
+ */
+public class IsInstanceOf implements UnaryPredicate {
+    private final Class<?> match;
+
+    public IsInstanceOf(Class<?> match) {
+        this.match = match;
+    }
+
+    public boolean execute(Object arg) {
+        return match.isAssignableFrom(arg.getClass());
+    }
+}
+
