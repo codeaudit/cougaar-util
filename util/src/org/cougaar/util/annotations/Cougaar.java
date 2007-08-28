991de6664b07f238671db9d0989a1531836a97c3
rshapiro <rshapiro>
2007-08-28 15:09:40 +0000

    Initial pass at execution annotation

--- util/src/org/cougaar/util/annotations/Cougaar.java
+++ util/src/org/cougaar/util/annotations/Cougaar.java
@@ -19,6 +19,8 @@ import org.cougaar.util.Arguments;
  * live elsewhere (eg {@link ParameterAnnotations}).
  */
 public class Cougaar {
+    // Plugin and Component parameter annotatons
+    
     /**
      * Can't use 'null' in annotation attributes, so use this instead
      */ 
@@ -28,6 +30,22 @@ public class Cougaar {
      * This is used to indicate that the field should be left as is
      */
     public static final String NO_VALUE = "###no-value###";
+    
+    public static enum ParamGroupRole {
+        MEMBER, OWNER
+    }
+
+    public static enum ParamGroupIterationPolicy {
+        ROUND_ROBIN, FIRST_UP, CLOSEST, RANDOM;
+    
+        // Default is to restrict the arguments to the
+        // given members, and then split it.
+        // 
+        // TODO: Specialize this per policy
+        List<Arguments> split(Arguments arguments, Set<String> members) {
+            return new Arguments(arguments, null, null, members).split();
+        }
+    }
     
     @Retention(RetentionPolicy.RUNTIME)
     public @interface Param {
@@ -49,20 +67,27 @@ public class Cougaar {
         Cougaar.ParamGroupIterationPolicy policy() default Cougaar.ParamGroupIterationPolicy.FIRST_UP;
     }
 
-    public static enum ParamGroupRole {
-        MEMBER, OWNER
-    }
-
-    public static enum ParamGroupIterationPolicy {
-        ROUND_ROBIN, FIRST_UP, CLOSEST, RANDOM;
     
-        // Default is to restrict the arguments to the
-        // given members, and then split it.
-        // 
-        // TODO: Specialize this per policy
-        List<Arguments> split(Arguments arguments, Set<String> members) {
-            return new Arguments(arguments, null, null, members).split();
-        }
+    // Execution annotations
+    
+    public static enum BlackboardOp {
+        ADD,
+        REMOVE,
+        MODIFY
     }
+    
+    public static enum BlackboardEvt {
+        BLACKBOARD,
+        ALARM,
+        TODO
+    }
+    
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Execute {                                                     
+        BlackboardEvt from() default BlackboardEvt.BLACKBOARD;        
+        BlackboardOp[] on();                                 
+        String when() default "";  //The name of a predicate method             
+        Class<?> isa() default Object.class;   // simple 'instanceof' predicate                                
+    }                                                                           
 
 }
