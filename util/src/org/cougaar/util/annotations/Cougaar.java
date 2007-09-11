76328a9b338fe6f617cd4d82f9ac49b36216987d
rshapiro <rshapiro>
2007-09-11 13:44:25 +0000

    Deprecate Cougaar.Predicate, prefer matching based on method names.

--- util/src/org/cougaar/util/annotations/Cougaar.java
+++ util/src/org/cougaar/util/annotations/Cougaar.java
@@ -9,7 +9,6 @@ package org.cougaar.util.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-
 /**
  * Use this class to define COUGAAR-specific annotations. The worker methods for
  * each annotation type should live elsewhere (eg {@link Argument}).
@@ -26,7 +25,6 @@ public class Cougaar {
      * This is used to indicate that the field should be left as is
      */
     public static final String NO_VALUE = "###no-value###";
-    
 
     /**
      * This annotation should be attached to a public data member to initialize
@@ -66,30 +64,38 @@ public class Cougaar {
     @Retention(RetentionPolicy.RUNTIME)
     public @interface Resolver {
     }
-    
-    
+
     // Obtaining services, TBD
     public @interface Service {
     }
-    
+
     // Execution annotations
-    
+
     /**
-     * Attaching this kind annotation to a public method will cause that method
-     * to be invoked once per item per {@link Subscribe.ModType} collection, for
-     * a given IncrementalSubscription. These invocations will happen in the
-     * plugin's execute context (ie, in blackboard transaction). These methods
-     * must be a public and should take exactly one parameter. The return value
-     * is ignored, so a 'void' return type is almost always the right choice.
+     * Attaching this kind annotation to a public method will create an
+     * IncrementalSubscription through the blackboard service, and cause the
+     * annotated method to be invoked for each item in each of the collections
+     * of that subscription mentioned in in {@link #on} clause. These
+     * invocations will happen in the plugin's execute thread, in a
+     * blackboard transaction.
      * 
      * By default, the method will be invoked on objects whose class matches the
-     * class of the method's parameter.
+     * class of the method's single parameter (it must take only one). In other
+     * words, the class of the parameter implicitly filters the subscription to
+     * items of that type. For further filtering, a value for the
+     * {@link #when} clause can be provided. This value should be name of a
+     * predicate method, ie, a public method on the same class that returns a
+     * boolean and takes a single parameter whose class matches that of the
+     * parameter of the annotated method.
      * 
-     * If a {@link #when} is specified, the plugin should also define a method
-     * with the same signature that returns a boolean, and that has a
-     * {@link Predicate} with a matching {@link Predicate#when}. This predicate
-     * method is used to further filter the set of items that will be passed to
-     * the execute method.
+     * For now, predicate methods can also be defined using the
+     * {@link Predicate} annotation. In this case, the {@link #when} clause
+     * should match the the {@link Predicate#when}. But Predicate annotations
+     * are deprecated and might well be dropped; their use is not
+     * recommended.
+     * 
+     * The return value of methods with an Execute annotation is ignored, so a
+     * 'void' return type is almost always the right choice.
      * 
      */
     @Retention(RetentionPolicy.RUNTIME)
@@ -100,22 +106,27 @@ public class Cougaar {
         Subscribe.ModType[] on();
 
         /**
-         * @return the name of a {@link Predicate}
+         * @return the name of a predicate method, or a name that matches some
+         *         {@link Predicate#when} on the same class}
          */
-        String when() default NO_VALUE; 
+        String when() default NO_VALUE;
     }
 
     /**
-     * Attaching this kind of annotation to a public method causes it to
-     * be used as a filter for an {@link Execute} method with a matching
-     * {@link Execute#when}.
+     * Attaching this kind of annotation to a public method causes it to be used
+     * as a filter for an {@link Execute} method with a matching {@link Execute#when}.
      * 
-     * A predicate method must be a public, must return a boolean and
-     * must take exactly one argument.  Further, the type of the argument
-     * should match that of the one argument of the corresonding 
-     * {@link Execute}.
+     * A predicate method must be a public, must return a boolean and must take
+     * exactly one argument. Further, the type of the argument should match that
+     * of the one argument of the corresonding {@link Execute}.
+     * 
+     * This annotation is deprecated.  Instead of annotating a method this way, 
+     * the preferred approach is simply to make the name a predicate method 
+     * match the value of the {@link Execute#when} clause.
+     * 
      */
     @Retention(RetentionPolicy.RUNTIME)
+    @Deprecated
     public @interface Predicate {
         String when() default NO_VALUE;
     }
